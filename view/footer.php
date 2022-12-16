b3e2f04890e8f7a55344fdc1fbe17803a78ed1f0
Aymen Mbarek <aymen.mbarek@mahdia.r-iset.tn>
2022-12-16 22:51:10 +0100

    all files done and it's the first demo

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> Demo
+        </div>
+        <strong>Copyright &copy; 2022 <a href="">Aymen , Iyed , Chaima</a>.</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
